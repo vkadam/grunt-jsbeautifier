074304159a69e00e536bdbea712cece1ccaa46a3
Vishal Kadam <vishal.4kadam@gmail.com>
2012-10-28 00:37:59 -0400

    Initial commit. 100% working with basic requirement

---
+++ README.md
@@ -0,0 +1,28 @@
+# grunt-jsbeautifier
+
+jsbeautifier.org for grunt
+
+## Getting Started
+Install this grunt plugin next to your project's [grunt.js gruntfile][getting_started] with: `npm install grunt-jsbeautifier`
+
+Then add this line to your project's `grunt.js` gruntfile:
+
+```javascript
+grunt.loadNpmTasks('grunt-jsbeautifier');
+```
+
+[grunt]: http://gruntjs.com/
+[getting_started]: https://github.com/gruntjs/grunt/blob/master/docs/getting_started.md
+
+## Documentation
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt][grunt].
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2012 Vishal Kadam  
+Licensed under the MIT license.
---
+++ tasks/jsbeautifier.js
@@ -0,0 +1,42 @@
+/*
+ * grunt-jsbeautifier
+ * https://github.com/vkadam/grunt-jsbeautifier
+ *
+ * Copyright (c) 2012 Vishal Kadam
+ * Licensed under the MIT license.
+ */
+
+module.exports = function(grunt) {
+    "use strict";
+    // Please see the grunt documentation for more information regarding task and
+    // helper creation: https://github.com/gruntjs/grunt/blob/master/docs/toc.md
+    // ==========================================================================
+    // TASKS
+    // ==========================================================================
+    grunt.registerTask('jsbeautifier', 'jsbeautifier.org for grunt', function() {
+        // Beautify specified files.
+        var jsBeautifierOrg = require('js-beautify'),
+            beautify = jsBeautifierOrg.js_beautify,
+            files = grunt.config(['jsbeautifier', 'files']),
+            tmp_opts = grunt.config(['jsbeautifier', 'options']);
+
+        if (tmp_opts) {
+            grunt.verbose.writeln(["Using options", JSON.stringify(tmp_opts)].join(" "));
+        }
+        var fileCount = 0;
+
+        grunt.file.expandFiles(files).forEach(function(filepath) {
+            var result = beautify(grunt.file.read(filepath), tmp_opts);
+            grunt.file.write(filepath, result);
+            fileCount++;
+        });
+        grunt.log.writeln(["Completed total", fileCount, "files."].join(" "));
+    });
+
+    // ==========================================================================
+    // HELPERS
+    // ==========================================================================
+    grunt.registerHelper('jsbeautifier', function() {
+        return 'jsbeautifier!!!';
+    });
+};
