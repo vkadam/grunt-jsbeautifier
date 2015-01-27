f0906d9637235f29ae301fbc8f5340e657f4aa22
Vishal Kadam <vishal.4kadam@gmail.com>
2015-01-26 21:56:00 -0500

    Removed nodeunit test cases

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,78 +12,6 @@ module.exports = function(grunt) {
         jsbeautifier: {
             default: {
                 src: ["<%= jshint.files %>", "!test/fixtures/**", "package.json"]
-            },
-            successWithForce: {
-                src: ["tmp/verifyMode/not-been-beautified.js", "tmp/verifyMode/not-been-beautified.css"],
-                options: {
-                    mode: "VERIFY_ONLY"
-                }
-            },
-            hasNotBeenBeautified: {
-                src: ["tmp/verifyMode/not-been-beautified.js", "tmp/verifyMode/not-been-beautified.css"],
-                options: {
-                    mode: "VERIFY_ONLY"
-                }
-            },
-            hasBeenBeautified: {
-                src: ["tmp/verifyMode/been-beautified.js"],
-                options: {
-                    mode: "VERIFY_ONLY"
-                }
-            },
-            fileMapping: {
-                src: ["tmp/fileMapping/not-beautified.js.erb",
-                    "tmp/fileMapping/not-beautified.css.erb", "tmp/fileMapping/not-beautified.html.erb"
-                ],
-                options: {
-                    js: {
-                        fileTypes: [".js.erb"],
-                        maxPreserveNewlines: 2
-                    },
-                    css: {
-                        fileTypes: [".css.erb"]
-                    },
-                    html: {
-                        fileTypes: [".html.erb"],
-                        preserveNewLines: true,
-                        maxPreserveNewlines: 1
-                    }
-                }
-            },
-            configFile: {
-                src: ["tmp/configFile/test.js",
-                    "tmp/configFile/test.css", "tmp/configFile/test.html"
-                ],
-                options: {
-                    config: "tmp/configFile/jsbeautifyrc.json"
-                }
-            },
-            configFileFlat: {
-                src: "<%= jsbeautifier.configFile.src %>",
-                options: {
-                    config: "tmp/configFile/jsbeautifyrc_flat.json"
-                }
-            },
-            configFileWithGruntFileOptions: {
-                src: "<%= jsbeautifier.configFile.src %>",
-                options: {
-                    config: "tmp/configFile/jsbeautifyrc_flat.json",
-                    js: {
-                        indentSize: 3
-                    },
-                    css: {
-                        indentSize: 5
-                    },
-                    html: {
-                        indentSize: 7
-                    }
-                }
-            },
-            dest: {
-                src: ["tmp/common/not-been-beautified.js"],
-                options: {
-                    dest: "dest"
-                }
             }
         },
         copy: {
@@ -95,9 +23,6 @@ module.exports = function(grunt) {
             }
         },
         clean: ["tmp", "dest"],
-        nodeunit: {
-            all: ["test/**/*_test.js"]
-        },
         mochaTest: {
             test: {
                 options: {
@@ -124,6 +49,6 @@ module.exports = function(grunt) {
     grunt.loadTasks("tasks");
 
     // By default, beautify, lint and run all tests.
-    grunt.registerTask("test", ["jshint", "copy", "nodeunit", "clean", "mochaTest"]);
+    grunt.registerTask("test", ["jshint", "copy", "clean", "mochaTest"]);
     grunt.registerTask("default", ["devUpdate", "test", "jsbeautifier:default"]);
 };
